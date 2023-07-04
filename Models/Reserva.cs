d416ca2d7bd2d4e2b4d1ebd093e9762ffbcc76b3
Thiago Lima <thiago.lima951@gmail.com>
2023-07-04 20:10:58 -0300

    Conclusão de Hospedagem!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,4 +1,5 @@
 namespace DesafioProjetoHospedagem.Models
+
 {
     public class Reserva
     {
@@ -17,14 +18,16 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if ( hospedes.Count <= Suite.Capacidade )
             {
                 Hospedes = hospedes;
+                
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+               throw new Exception("Capacidade menor do que o número de hóspedes");
             }
         }
 
@@ -37,7 +40,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,16 +48,18 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor -= Decimal.Divide(Decimal.Multiply(valor, 10), 100);
+                
             }
-
+            
             return valor;
+
         }
     }
 }
